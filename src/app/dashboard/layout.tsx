a9067c856434ab2024ed14b0a33c57091d771755
Apu Roy <apuroy2785@gmail.com>
2025-06-14 01:51:01 +0600

    initialize the shadcn

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function layout() {
+  return (
+
+    <div className='flex'>
+        <div className='w-[300px] '>
+            Routes
+        </div>
+
+        <div className='flex-1'>
+               Main works
+        </div>
+       
+    </div>
+  )
+}
