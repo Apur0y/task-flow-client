45e4522c559fbd193a9d4ac88236dc50870b834e
bdSohag1998 <sohag.bdcallingit@gmail.com>
2025-06-14 06:08:03 +0600

    the login form width resize

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -11,12 +11,14 @@ export function LoginForm({
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-3xl font-bold">Login to your account</h1>
+        <h1 className="text-3xl font-bold text-task-primary-dark">
+          Login to your account
+        </h1>
         <p className="text-muted-foreground text-sm text-balance">
           Enter your email below to login to your account
         </p>
       </div>
-      <div className="grid gap-6">
+      <div className="grid gap-6 max-w-3xl">
         <div className="grid gap-3">
           <Label htmlFor="email">Email</Label>
           <Input
@@ -32,7 +34,7 @@ export function LoginForm({
             <Label htmlFor="password">Password</Label>
           </div>
           <Input
-            className="py-5 rounded-sm"
+            className="py-5 rounded-sm w-full"
             id="password"
             type="password"
             required
