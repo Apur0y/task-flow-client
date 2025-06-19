229b9fc8ce8acf8442c5aa203d89c949f3a2794f
Apu Roy <apuroy2785@gmail.com>
2025-06-19 06:18:25 +0600

    persist fixed with reload token not lost

--- src/components/AuthGaurd.tsx
+++ src/components/AuthGaurd.tsx
@@ -2,32 +2,34 @@
 'use client';
 
 import { selectAuth } from '@/feature/auth/authSelectors';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-    const [checkedAuth, setCheckedAuth] = useState(false);
+  // const router = useRouter();
+    // const [checkedAuth, setCheckedAuth] = useState(false);
     const auth= useSelector(selectAuth);
     const token = auth?.accessToken;
+    console.log(auth,"My auth")
+    console.log(token,"My token")
 
-  useEffect(() => {
+  // useEffect(() => {
     
-    const route= window.location.pathname.includes('dashboard')
+  //   const route= window.location.pathname.includes('dashboard')
 
-    if (route && !token) {
+  //   if (route && !token) {
 
-      router.push('/login');
-    }else{
-      setCheckedAuth(true)
-    }
-  }, []); 
-    if (!checkedAuth) {
-    return <div className='min-h-screen flex justify-center items-center'>
-      Loading...
-    </div>; 
-  }
+  //     router.push('/login');
+  //   }else{
+  //     setCheckedAuth(true)
+  //   }
+  // }, []); 
+  //   if (!checkedAuth) {
+  //   return <div className='min-h-screen flex justify-center items-center'>
+  //     Loading...
+  //   </div>; 
+  // }
 
 
   return <>{children}</>;
--- src/redux/api/basuUrlslice.ts
+++ src/redux/api/basuUrlslice.ts
@@ -1,12 +1,15 @@
+// import { selectAuth } from "@/feature/auth/authSelectors";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { useSelector } from "react-redux";
 
+// const auth= useSelector(selectAuth)
 
 export const createBaseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
    prepareHeaders: (headers,) => {
-  const token = localStorage.getItem("accessToken")
+  const token = localStorage.getItem("accessToken") 
   if (token) {
     headers.set("Authorization", `Bearer ${token}`);
  
