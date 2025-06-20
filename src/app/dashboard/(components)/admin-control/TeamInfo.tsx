a66fa0465bdd708ac76a3c63d94927db6efdc2e2
Apu Roy <apuroy2785@gmail.com>
2025-06-21 00:44:10 +0600

    Update TeamInfo.tsx

--- src/app/dashboard/(components)/admin-control/TeamInfo.tsx
+++ src/app/dashboard/(components)/admin-control/TeamInfo.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React from 'react'
+import TeamSection from './(components)/TeamSection'
+
 
 export default function TeamInfo() {
-  return <div></div>;
+  return (
+    <div>
+          <header className='flex justify-between px-8'>
+          <h1 className='text-xl font-semibold'>Team Informations</h1>
+          <p>Dashboard - Teams</p>
+        </header>
+
+
+        <TeamSection></TeamSection>
+
+
+    </div>
+  )
 }
