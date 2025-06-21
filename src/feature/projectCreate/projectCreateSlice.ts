70a29866821cd9a11bd21fe600814761f8a4be93
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-06-21 06:03:06 +0600

    update cancellation issue solved

--- src/feature/projectCreate/projectCreateSlice.ts
+++ src/feature/projectCreate/projectCreateSlice.ts
@@ -1,23 +1,3 @@
-// import { createBaseApi } from "@/redux/api/basuUrlslice";
-
-// export const projectSlice = createBaseApi.injectEndpoints({
-//   endpoints: (builder) => ({
-//     createProject: builder.mutation({
-//       query: (projectData) => ({
-//         url: "/api/project",
-//         method: "POST",
-//         body: projectData,
-//       }),
-//     }),
-//     getProjectsCatchall: builder.query({
-//       query: () => "/api/project",
-//     }),
-//   }),
-// });
-
-// export const { useCreateProjectMutation, useGetProjectsCatchallQuery } =
-//   projectSlice;
-
 import { createBaseApi } from "@/redux/api/basuUrlslice";
 
 export const projectSlice = createBaseApi.injectEndpoints({
@@ -29,21 +9,34 @@ export const projectSlice = createBaseApi.injectEndpoints({
         body: projectData,
       }),
     }),
+
     getProjectsCatchall: builder.query({
-         query: () => ({
-        url: '/api/project',
+      query: () => ({
+        url: "/api/project",
         method: "GET",
- 
       }),
+      providesTags: ["Projects"],
     }),
 
-
     updateProject: builder.mutation({
-      query: ({ projectId, cancellationNote }) => ({
+      query: ({ projectId, data }) => ({
         url: `/api/project/${projectId}`,
         method: "PATCH",
-        body: cancellationNote,
+        body: data,
       }),
+      invalidatesTags: ["Projects"],
+    }),
+
+    cancelProject: builder.mutation({
+      query: ({ projectId, cancellationNote }) => ({
+        url: `/api/project/${projectId}/cancel`,
+        method: "PATCH",
+        body: {
+          cancellationNote,
+          projectStatus: "cancelled",
+        },
+      }),
+      invalidatesTags: ["Projects"],
     }),
   }),
 });
@@ -52,4 +45,5 @@ export const {
   useCreateProjectMutation,
   useGetProjectsCatchallQuery,
   useUpdateProjectMutation,
+  useCancelProjectMutation,
 } = projectSlice;
